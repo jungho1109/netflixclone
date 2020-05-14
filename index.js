7aa5db4c5b93271d3075dba7a29dd3d74af1df72
Jeongho Han <jungho1109@gmail.com>
2020-05-14 18:13:57 +0900

    package setting

--- index.js
+++ index.js
@@ -14,4 +14,12 @@ app.use(express.json({ extended: false }));
 app.use("/user", userRouter);
 app.use("/api", apiRouter);
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.listen(process.env.PORT || 5000, () => console.log(`Server is On`));
